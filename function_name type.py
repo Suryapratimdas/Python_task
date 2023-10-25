61d3518402f696db1afd24343aee417def757db8
Suryapratim Das <130431356+Suryapratimdas@users.noreply.github.com>
2023-10-25 14:46:22 +0530

    Update function_name type.py

--- function_name type.py
+++ function_name type.py
@@ -6,6 +6,36 @@ def  Addition():
     a = int(input("Enter no for a:"))
     b = int(input("Enter no for b:"))
     c =a+b
+    d =a-b
+    e =a*b
+    f =a/b
     print("addition=",c)
-Addition()
+    
+def  Subtraction():
+    a = int(input("Enter no for a:"))
+    b = int(input("Enter no for b:"))
+    c=a-b
+    print("subtraction=",c)
+def  Multiplication():
+    a = int(input("Enter no for a:"))
+    b = int(input("Enter no for b:"))
+    c=a*b
+    print("multiplication=",c)
+def  Division():
+    a = int(input("Enter no for a:"))
+    b = int(input("Enter no for b:"))
+    c=a/b
+    print("division=",c)  
+while True:
+    c =input("enter your choice=")
+    if c=="1":
+        Addition()
+    elif c=="2":
+        Subtraction()
+    elif c=="3":
+        Multiplication()
+    elif c=="4":
+        Division()
+    else:
+        print("exit")
 

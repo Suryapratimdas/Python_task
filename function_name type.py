792e1003c44164b7c1e74882038f4394a6a6e723
Suryapratim Das <130431356+Suryapratimdas@users.noreply.github.com>
2023-10-25 14:26:29 +0530

    Add files via upload

---
+++ function_name type.py
@@ -0,0 +1,11 @@
+def  print_name():
+    name="SURYA"
+    print(name)
+print_name()    
+def  Addition():
+    a = int(input("Enter no for a:"))
+    b = int(input("Enter no for b:"))
+    c =a+b
+    print("addition=",c)
+Addition()
+
